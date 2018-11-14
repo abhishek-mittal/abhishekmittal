d96281770437c134829a3cb382ea0fa4ef678f03
Abhishek Mittal <abhishekmittaloffice@gmail.com>
2018-11-14 13:59:24 +0530

    done

--- components/Schene.js
+++
@@ -1,9 +0,0 @@
-import css from './../styles/schene.scss';
-
-const Schene = props => (
-    <div className={css.canvas}>
-        <div>{props.children}</div>
-    </div>
-);
-
-export default Schene;
